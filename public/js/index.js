44f1e9aab778756f658e5e40b43b17bde444d8a8
hetal <hetal93hasmukh@gmail.com>
2019-07-25 11:42:15 +0530

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,13 @@ socket.on("disconnect",()=>{
 
 socket.on("newMessage",function(message){
     // console.log("got message",message);
-    jQuery("#messages").prepend(`<li>${message.from}:${message.text}</li>`);
+    jQuery("#messages").prepend(`<li><span style='font-weight:bold;'>${message.from}:</span> ${message.text}</li>`);
 });
 
 socket.on("newLocationMessage",function(message){
     var li=jQuery("<li></li>");
     var a=jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}:`);
+    li.html(`<span style='font-weight:bold;'>${message.from}:</span>  `);
     a.attr("href",message.url);
     li.append(a);
     jQuery('#messages').prepend(li);
@@ -36,11 +36,12 @@ socket.on("newLocationMessage",function(message){
 
 jQuery('#message-form').on("submit",(e)=>{
     e.preventDefault();
+    var messageTextBox=jQuery('[name=message]');
     socket.emit("createMessage",{
         from:"User",
-        text:jQuery('[name=message]').val()
+        text:messageTextBox.val()
     },function(){
-        console.log("Got it");
+        messageTextBox.val('');
     });
 });
 
@@ -48,13 +49,17 @@ var locationButton=jQuery("#send-location");
 locationButton.on('click',()=>{
     if(!navigator.geolocation)
         return alert("Geolocation is not supported by your browser");
+    locationButton.attr('disabled','disabled').text('sending location...');
     navigator.geolocation.getCurrentPosition(position=>{
        socket.emit('createLocationMessage',{
         latitude:position.coords.latitude,
         longitude:position.coords.longitude
        });
-    },()=>{
+     },()=>{
+        //locationButton.removeAttr('disabled');
         alert("unable to fetch geolocation");
+        //locationButton.text('v');
     });
+    locationButton.removeAttr('disabled').text('Send Location');
 });
 //https://www.google.com/maps?q=21.1591168,72.76871679999999
